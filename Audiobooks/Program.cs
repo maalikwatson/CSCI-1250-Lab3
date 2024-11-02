3e1138279cc0269d0a4014ca6272012497a3f8a6
watsmaa <watsmaa@outlook.com>
2024-11-02 09:31:31 -0400

    added documentation to Audiobooks program.

--- Audiobooks/Program.cs
+++ Audiobooks/Program.cs
@@ -4,12 +4,14 @@
     {
         static void Main(string[] args)
         {
+            // prompt user to input base package and total books read
             Console.Write("Enter the letter of the Audiobooks package you purchased (A,B,or C): ");
             char packageLetter = Convert.ToChar(Console.ReadLine());
 
             Console.Write("Enter the number of Audiobooks that you've read: ");
             int numBooks = Convert.ToInt32(Console.ReadLine());
 
+            // constant values for base packages A,B,C & the additional fees per book read after limit is reached
             const double BaseA = 9.95;
             const double BooksA = 2.00;
 
@@ -20,7 +22,7 @@
 
             double totalCharges = 0.00;
 
-
+            // selection statement that accounts for upper/lowercase input and calculations required to determine total charges
             if (packageLetter == 'A' && numBooks <= 10 || packageLetter == 'a' && numBooks <= 10)
             {
                 totalCharges = BaseA;
@@ -42,10 +44,11 @@
                 totalCharges = BaseC;
             }
 
-
+            // creates new line and prints total charges to console
             Console.WriteLine("");
             Console.WriteLine("Total charges for " + numBooks + " books for plan " + packageLetter + " is " + Math.Round(totalCharges, 2));
 
+            // press any key to terminate program
             Console.ReadKey();
         }
     }
