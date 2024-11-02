10a30d465292a61491eda15b216d31d303d48636
watsmaa <watsmaa@outlook.com>
2024-11-02 09:26:26 -0400

    Add project files.

---
+++ Audiobooks/Program.cs
@@ -0,0 +1,52 @@
+﻿namespace Audiobooks
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the letter of the Audiobooks package you purchased (A,B,or C): ");
+            char packageLetter = Convert.ToChar(Console.ReadLine());
+
+            Console.Write("Enter the number of Audiobooks that you've read: ");
+            int numBooks = Convert.ToInt32(Console.ReadLine());
+
+            const double BaseA = 9.95;
+            const double BooksA = 2.00;
+
+            const double BaseB = 13.95;
+            const double BooksB = 1.00;
+
+            const double BaseC = 19.95;
+
+            double totalCharges = 0.00;
+
+
+            if (packageLetter == 'A' && numBooks <= 10 || packageLetter == 'a' && numBooks <= 10)
+            {
+                totalCharges = BaseA;
+            }
+            else if (numBooks > 10)
+            {
+                totalCharges = ((numBooks - 10) * BooksA) + BaseA;
+            }
+            if (packageLetter == 'B' && numBooks <= 20 || packageLetter == 'b' && numBooks <= 20)
+            {
+                totalCharges = BaseB;
+            }
+            else if (numBooks > 20)
+            {
+                totalCharges = ((numBooks - 20) * BooksB) + BaseB;
+            }
+            if (packageLetter == 'C' || packageLetter == 'c')
+            {
+                totalCharges = BaseC;
+            }
+
+
+            Console.WriteLine("");
+            Console.WriteLine("Total charges for " + numBooks + " books for plan " + packageLetter + " is " + Math.Round(totalCharges, 2));
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ AverageScore/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace AverageScore
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // prompt user to input three homework scores
+            Console.Write("Please enter score 1: ");
+            double score1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Please enter score 2: ");
+            double score2 = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Please enter score 3: ");
+            double score3 = Convert.ToDouble(Console.ReadLine());
+
+            // calculate the average of test scores
+            double average = (score1 + score2 + score3) / 3;
+
+            // print average to the console
+            Console.WriteLine("Your average score is " + Math.Round(average, 2) + ".");
+
+            // if statement that prints string data under explict conditions
+            if (average > 95.00)
+            {
+                Console.WriteLine("That's a great score!");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
